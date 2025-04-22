3ae29258b3691fb7875e78bf7ecd1a11d4b1ae80
pnz1990 <rafaelpanazzo@gmail.com>
2025-04-22 12:05:26 -0700

    Update version to 1.2.0 to force new deployment

--- js/game.js
+++ js/game.js
@@ -1,15 +1,15 @@
-// game.js - Main game file that loads all modules
+// game.js - Main game file that loads all modules - v1.2.0
 
 // Load all game modules in the correct order
-document.write('<script src="js/config.js"></script>');
-document.write('<script src="js/utils.js"></script>');
-document.write('<script src="js/assets.js"></script>');
-document.write('<script src="js/player.js"></script>');
-document.write('<script src="js/enemies.js"></script>');
-document.write('<script src="js/boss.js"></script>');
-document.write('<script src="js/levels.js"></script>');
-document.write('<script src="js/input.js"></script>');
-document.write('<script src="js/ui.js"></script>');
-document.write('<script src="js/effects.js"></script>');
-document.write('<script src="js/engine.js"></script>');
-document.write('<script src="js/main.js"></script>');
+document.write('<script src="js/config.js?v=1.2.0"></script>');
+document.write('<script src="js/utils.js?v=1.2.0"></script>');
+document.write('<script src="js/assets.js?v=1.2.0"></script>');
+document.write('<script src="js/player.js?v=1.2.0"></script>');
+document.write('<script src="js/enemies.js?v=1.2.0"></script>');
+document.write('<script src="js/boss.js?v=1.2.0"></script>');
+document.write('<script src="js/levels.js?v=1.2.0"></script>');
+document.write('<script src="js/input.js?v=1.2.0"></script>');
+document.write('<script src="js/ui.js?v=1.2.0"></script>');
+document.write('<script src="js/effects.js?v=1.2.0"></script>');
+document.write('<script src="js/engine.js?v=1.2.0"></script>');
+document.write('<script src="js/main.js?v=1.2.0"></script>');
